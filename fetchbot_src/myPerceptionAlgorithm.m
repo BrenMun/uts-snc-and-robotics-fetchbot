a52c3c4d90497597c55b0d597f64ac3448c6b112
brenmun00@gmail.com <brenmun00@gmail.com>
2022-04-12 13:26:01 +1000

    added matlab-ros template files

---
+++ fetchbot_src/myPerceptionAlgorithm.m
@@ -0,0 +1,7 @@
+function perceptionOutput = myPerceptionAlgorithm(recvData,param)
+% Placeholder perception algorithm
+% Copyright 2017 The MathWorks, Inc.
+
+    perceptionOutput = recvData / param; 
+
+end
