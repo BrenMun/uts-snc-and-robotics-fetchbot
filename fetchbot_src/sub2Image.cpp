57845cee855c3f38aaee3aa1e2cab177b38a0957
brenmun00@gmail.com <brenmun00@gmail.com>
2022-04-16 19:18:02 +1000

    spin once

--- fetchbot_src/sub2Image.cpp
+++ fetchbot_src/sub2Image.cpp
@@ -1,16 +1,10 @@
 #include <ros/ros.h>
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/image_encodings.h>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
 #include "ImageConverter.h"
-#include <filesystem>
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "image_converter");
   ImageConverter ic;
-  while(!std::filesystem::exists("data/my_image0.png")) ros::spin();
+  ros::spinOnce();
   return 0;
 }
