b2d2047a4f2913c29c344c46b8433520e499de0d
brenmun00@gmail.com <brenmun00@gmail.com>
2022-04-16 21:41:47 +1000

    removed file_exists()

--- fetchbot_src/sub2Image.cpp
+++ fetchbot_src/sub2Image.cpp
@@ -4,15 +4,6 @@
 #include <stdbool.h>
 #include <string>
 
-bool file_exists (std::string filename) {
-  std::string s(filename);
-  char p[s.length()]; 
-  int i;
-  for (i = 0; i < sizeof(p); i++) p[i] = s[i];
-  struct stat   buffer;   
-  return (stat (p, &buffer) == 0);
-}
-
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "image_converter");
