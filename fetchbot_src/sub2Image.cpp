1fa23618d644b8beb88bcc2444b474f05b7b3418
brenmun00@gmail.com <brenmun00@gmail.com>
2022-04-16 22:57:18 +1000

    save an image (working)

--- fetchbot_src/sub2Image.cpp
+++ fetchbot_src/sub2Image.cpp
@@ -4,18 +4,22 @@
 
 int main(int argc, char** argv)
 {
+  //initialise ros
   ros::init(argc, argv, "image_converter");
+  //subscribe to head camera topic and save an image in data folder
   ImageConverter ic;
   
+  //trys to open image and returns true if the file exists
   std::ifstream ifile;
   ifile.open("../data/my_image0.png");
   
+  //while the image isn't saved
   while(!ifile){
+    //spin once
     ros::spinOnce();
+    //try to open image
     ifile.open("../data/my_image0.png");
-    std::cout << "file not found\n";
   }
-  std::cout << "file found\n";
   ros::shutdown();
   return 0;
 } 
