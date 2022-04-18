0377c1fa47a514b2e638348cdcdde47f762eacff
BrenMun <brenmun@gmail.com>
2022-04-18 08:48:40 +0000

    look at table, opencv set to 3.2

--- fetchbot_src/testHeadControl.cpp
+++ fetchbot_src/testHeadControl.cpp
@@ -5,7 +5,8 @@ int main(int argc, char** argv)
 {
   //init the ROS node
   ros::init(argc, argv, "robot_driver");
-
+  //initialise robot head class
   RobotHead head;
-  head.shakeHead(3);
+  // look at (x,y,z) position in a given frame
+  head.lookAt("base_link", 1, 0, 0.4);
 }
