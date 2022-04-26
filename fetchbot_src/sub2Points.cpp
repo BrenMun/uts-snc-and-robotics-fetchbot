c488a03590fbfe479ba815d05152bc0c6aafee99
BrenMun <brenmun@gmail.com>
2022-04-26 14:30:37 +0000

    get the 3d point at image coordinates

--- fetchbot_src/sub2Points.cpp
+++ fetchbot_src/sub2Points.cpp
@@ -2,20 +2,20 @@
 #include <pcl_ros/point_cloud.h>
 #include <pcl/point_types.h>
 #include <boost/foreach.hpp>
+#include <iostream>
 
-typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
-
-void callback(const PointCloud::ConstPtr& msg)
-{
-  printf ("Cloud: width = %d, height = %d\n", msg->width, msg->height);
-  BOOST_FOREACH (const pcl::PointXYZ& pt, msg->points)
-    printf ("\t(%f, %f, %f)\n", pt.x, pt.y, pt.z);
+void cloudCb(const sensor_msgs::PointCloud2ConstPtr & cloud) {
+  pcl::PointCloud<pcl::PointXYZ> depth;
+  pcl::fromROSMsg(*cloud, depth);
+  pcl::PointXYZ p1 = depth.at(233, 275);
+  pcl::PointXYZ 
+  std::cout << "Point: (" << p1.x << ", " << p1.y << ", " << p1.z << ")\n";
 }
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "sub_pcl");
   ros::NodeHandle nh;
-  ros::Subscriber sub = nh.subscribe<PointCloud>("/head_camera/depth_registered/points", 1, callback);
+  ros::Subscriber sub = nh.subscribe<sensor_msgs::PointCloud2>("/head_camera/depth_registered/points", 1, &cloudCb);
   ros::spin();
 }
