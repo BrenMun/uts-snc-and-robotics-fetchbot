1c2794b0df80b0deb6994e60eb775cf35fd56ad1
brenmun00@gmail.com <brenmun00@gmail.com>
2022-05-02 19:37:25 +1000

    remove unused code and sub to /target_point

--- fetchbot_src/matlab/RobotTemplateClass.m
+++ fetchbot_src/matlab/RobotTemplateClass.m
@@ -1,24 +1,12 @@
 classdef RobotTemplateClass < handle
-    % ROBOTTEMPLATECLASS Contains Robot Information
-    % Copyright 2017 The MathWorks, Inc.
-    
     properties
         % ROS publishers and subscribers
-        MySub = [];
-        MyPub = [];
-        PubMsg = [];
-        
+        MySub = []; MyPub = []; PubMsg = [];
         % Perception algorithm   
         PerceptionParam = 1;
-        
         % Control algorithm
-        ControlInputs = 0; 
-        ControlOutputs = 0;
-        ControlParams = struct('gain',1);
-        
-        % Visualization
-        MyFig = [];
-        MyAxes = [];
+        ControlInputs = 0; ControlOutputs = 0; ControlParams = struct('gain',1);
+        % Current time
         CurrentTime = 0;
     end
     
@@ -26,50 +14,32 @@ classdef RobotTemplateClass < handle
         %% CONSTRUCTOR
         function obj = RobotTemplateClass(ipAddr)
             % Connect to ROS master at the IP address specified
-            rosshutdown;
-            rosinit(ipAddr);
-            
-            % Create subscribers
-            obj.MySub = ...
-                rossubscriber('/my_sub_topic','geometry_msgs/Point');
-            
-            % Create publisher
-            [obj.MyPub,obj.PubMsg] = ... 
-                rospublisher('/my_pub_topic','geometry_msgs/Point'); 
-            
-            % Start visualization
-            obj.MyFig = figure;
-            obj.MyAxes = axes(obj.MyFig);
-            title(obj.MyAxes,'Control Output')
-            xlabel(obj.MyAxes,'Time [s]')
-            hold(obj.MyAxes,'on');
+            rosshutdown; rosinit(ipAddr);
+            % Create subscriber (subscribes to target_point of type geometry_msgs::PointStamped)
+            obj.MySub = rossubscriber('/target_point','geometry_msgs/PointStamped');
+            % Create publisher (no publisher yet)
+             %[obj.MyPub,obj.PubMsg] = rospublisher('/my_pub_topic','geometry_msgs/Point'); 
         end     
         
         %% PERCEPTION ALGORITHM
-        % Receives sensor data, runs perception algorithm, and visualizes
+        % Receives sensor data, runs perception algorithm
         function perceptionFcn(obj,receivedMsg)           
             if isempty(receivedMsg)
-               receivedData = rand; % If message is empty, assign random number
+                disp 'message is empty'
             else
-               receivedData = receivedMsg.Z;
+                receivedData = receivedMsg.Point;
+                obj.ControlInputs = myPerceptionAlgorithm(receivedData,obj.PerceptionParam);  
             end
-            
-            % Perception algorithm
-            obj.ControlInputs = ... 
-                myPerceptionAlgorithm(receivedData,obj.PerceptionParam);            
         end
         
         %% CONTROL ALGORITHM
         % Runs control algorithm and publishes ROS commands
         function controlFcn(obj)            
-             % Control algorithm
-            obj.ControlOutputs = ... 
-                myControlAlgorithm(obj.ControlInputs,obj.ControlParams);
-            
+            % Control algorithm
+            obj.ControlOutputs = myControlAlgorithm(obj.ControlInputs,obj.ControlParams);
             % Package ROS message and send to the robot
             obj.PubMsg.X = obj.ControlOutputs;
-            send(obj.MyPub,obj.PubMsg);    
-            
+            send(obj.MyPub,obj.PubMsg);
         end       
     end
     
--- fetchbot_src/matlab/myPerceptionAlgorithm.m
+++ fetchbot_src/matlab/myPerceptionAlgorithm.m
@@ -1,7 +1,5 @@
 function perceptionOutput = myPerceptionAlgorithm(recvData,param)
 % Placeholder perception algorithm
-% Copyright 2017 The MathWorks, Inc.
-
-    perceptionOutput = recvData / param; 
-
-end
+    perceptionOutput = recvData; %/ param; 
+    disp(perceptionOutput)
+end 
