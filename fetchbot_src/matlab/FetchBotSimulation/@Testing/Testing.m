d0ddcf98bdbc22a3780fb0099e772931e2fde806
Ianchivers <ianchivers72@gmail.com>
2022-05-04 13:47:45 +1000

    Finially got DH fixed

--- fetchbot_src/matlab/FetchBotSimulation/@Testing/Testing.m
+++ fetchbot_src/matlab/FetchBotSimulation/@Testing/Testing.m
@@ -112,21 +112,22 @@ clear all
 
 % https://www.google.com/url?sa=i&url=http%3A%2F%2Fragazzini.me.vt.edu%2Fpdf%2FLearning_IK_ID_GAN_RAS_2020.pdf&psig=AOvVaw1Rba0hDu8iqFIiWcYX9SmQ&ust=1650853547949000&source=images&cd=vfe&ved=0CA0QjhxqFwoTCPC9kO7Sq_cCFQAAAAAdAAAAABAm
 
-L0 = 0.1;  % x
-D1 = 0.2;
-L1 = 0.1;
-L2 = 0.1;   % x
-L3 = 0.1;
-L4 = 0.1;
-L5 = 0.1;
-L6 = 0.1;
-L7 = 0.1;
-L8 = 0.1;
-L9 = 0.1;
+L0 = 0.110;  % x
+D1 = 0.0;  % 
+L1 = 0.1197;  % 
 
-    L(1) = Link([pi     L1+D1   L0         0      1]); % PRISMATIC Link
-    L(2) = Link([0      L3      L2        -pi/2   0]);
-    L(3) = Link([0      0       0         -pi/2   0]);
+L2 = 0.1163;  % Shoulder pan joint
+L3 = 0.17181;  % Shoulder lift joint
+L4 = 0.18093;  % Upper arm roll joint
+L5 = 0.1658;  % elbow flex joint
+L6 = 0.15675;  % forarm roll joint
+L7 = 0.1166;  % wrist flex joint
+L8 = 0.04224;  % wrist roll joint
+L9 = 0.12193;  % gripper joint
+
+    L(1) = Link([pi     L1+D1   L0         0      1]); % PRISMATIC Link shulder pan
+    L(2) = Link([0      L3      L2        -pi/2   0]); % SHoulder lift
+    L(3) = Link([0      0       0         -pi/2   0]); % upper arm rol joint
     L(4) = Link([0      L4+L5   0          pi/2   0]);
     L(5) = Link([0      0       0         -pi/2   0]);
     L(6) = Link([0      L6+L7   0          pi/2	  0]);
@@ -138,17 +139,16 @@ L9 = 0.1;
     L(1).qlim = [0 0.400];
     L(2).qlim = [-23/45*pi, 23/45*pi];
     L(3).qlim = [-7/18*pi, 29/60*pi];
-    %L(4).qlim = [-360 360]*pi/180; %continuous not sure how to do that
+    L(4).qlim = [-360 360]*pi/180; %continuous not sure how to do that
     L(5).qlim = [-43/60*pi, 43/60*pi];
-    %L(6).qlim = [-360 360]*pi/180; %continuous
+    L(6).qlim = [-360 360]*pi/180; %continuous
     L(7).qlim = [-25/36*pi, 25/36*pi ];
-    %L(8).qlim = [-360 360]*pi/180; %continuous
+    L(8).qlim = [-360 360]*pi/180; %continuous
 
-    %L(3).offset = -pi/2;
-    %L(5).offset = -pi/2;
+    L(3).offset = -pi/2;
+    L(5).offset = -pi/2;
     
     fetchBot = SerialLink([L(1) L(2) L(3) L(4) L(5) L(6) L(7) L(8)]);
-
     fetchBot.base = transl(0,0,0);
                                     
     workspace = [-1.5 1.5 -1.5 1.5 -1.5 1.5]; 
@@ -158,7 +158,9 @@ L9 = 0.1;
     hold on
     
     fetchBot.teach
-
+    %Add table like you added the cube in lab 5
+    % add is collision and avoid collison in
+    %change from 3 dof to 8? dof
 
  %% https://au.mathworks.com/help/robotics/ug/check-for-manipulator-self-collisions-using-collision-meshes.html
 clear 
