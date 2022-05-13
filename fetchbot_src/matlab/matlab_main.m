b627f507ce41875e87ac77867c0ccaf96b931017
brenmun00@gmail.com <brenmun00@gmail.com>
2022-05-14 01:18:24 +1000

    main updated

--- fetchbot_src/matlab/matlab_main.m
+++ fetchbot_src/matlab/matlab_main.m
@@ -1,21 +1,38 @@
-clear; clc; close all; 
+%% Assignment 2
+clf; clc; clear all;
 
-%% SETUP
-robot = RobotTemplateClass(getenv("ROS_IP"));
+% Variables
+taskCompleted = false; %Boolean for task completed 
+workspace = [-0.2 1.7 -1 1 0.2 1.5]; % workspace for matlab simulation
+centerpnt = [1,0,0.25];  % Centre point for table proxy in matlab
+binPoint = [-0.2,0.5,0]; % bin centre point 
 
-%% CONTROL LOOP
-tic
-currentTime = 0;
-while(currentTime < 10)
-    %% 1: SENSE, 2: PROCESS and 3: CONTROL
-    % Get latest data from ROS subscribers
-    % Run perception and control algorithms, which use received data and 
-    % control parameters to produce some output.
-    % Package and send control outputs as ROS messages
-    robot.perceptionFcn(robot.subPoint.LatestMessage, robot.subCloud.LatestMessage); 
-%     robot.controlFcn 
-    pause(0.1)
+% estop functionality
+eStop = false; 
+
+% Add starting Poses for Fetch Robot
+fetchBase = transl(0,0,0.7) *trotz(pi);
+
+% Creates object conenct to ROS node for head camera information
+headCamera = RobotTemplateClass(getenv("ROS_IP"));
+
+% Generate the Simulation
+simulation = Simulation(fetchBase, workspace, centerpnt, binPoint, headCamera);
+
+% Grab object location from camera in ROS
+headCamera.perceptionFcn(headCamera.subPoint.LatestMessage, headCamera.subCloud.LatestMessage);
+object = headCamera.targetPoint;
+
+% add object to matlab simulation
+simulation.addObject(1,object)
+
+%% Recycle Object
+while (taskCompleted == false && eStop == false)
+    simulation.Recycle(simulation.robotFetch);
+    if (simulation.robotFetch.taskcompleted == true)
+        taskCompleted = true;
+        disp("Object Recycled");
+    end
 end
 
-% shut down node after loop
 rosshutdown
