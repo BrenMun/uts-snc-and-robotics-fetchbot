0e974562d9d371ce19156535cf6368f89a41834e
brenmun00@gmail.com <brenmun00@gmail.com>
2022-05-14 01:18:46 +1000

    working

--- fetchbot_src/matlab/solveRMRC.m
+++ fetchbot_src/matlab/solveRMRC.m
@@ -1,11 +1,11 @@
 % RMRC based on robotics lab 9 
-function [qMatrix, posError, angleError] = solveRMRC(robot, point1, point2, q0, totalTime, deltaT)
+function [qMatrix, posError, angleError] = solveRMRC(robot, pointA, pointB, q0, totalTime, deltaT, distance)
     %% set up parameters
     numJoints = length(q0);                                                 % Number of joints
     t = totalTime;                                                          % Total time
     steps = t/deltaT;                                                       % Number of steps based on time
-    epsilon = 0.1;                                                          % Manipulability threshold value
-    W = eye(numJoints);                                                     % Weighting matrix for velocity vector
+    epsilon = 0.5;                                                          % Manipulability threshold value
+    W = diag([1 1 1 0.1 0.1 0.1]);                                          % Weighting matrix for velocity vector
     
     %% allocate array data
     m = zeros(steps,1);                                                     % measure of manipulability
@@ -17,32 +17,28 @@ function [qMatrix, posError, angleError] = solveRMRC(robot, point1, point2, q0, 
     
     %% set up trajectory, initial pose
     s = lspb(0,1,steps);                                                    % trapezoidal trajectory
-    distZ = 0.3;                                                            % desired distance from end-effector to point in Z
-    x1 = point1(1); y1 = point1(2); z1 = point1(3) + distZ;                 % xyz of point1
-    x2 = point2(1); y2 = point2(2); z2 = point2(3) + distZ;                 % xyz of point2
-
+    distZ = distance;                                                       % desired distance from end-effector to point in Z
+    x0 = pointA(1); y0 = pointA(2); z0 = pointA(3);                         % xyz of point1
+    x1 = pointB(1); y1 = pointB(2); z1 = pointB(3) + distZ;                 % xyz of point2
     for i=1:steps
-        pointTraj(1,i) = (1-s(i))*x1 + s(i)*x2;                             % points in x
-        pointTraj(2,i) = (1-s(i))*y1 + s(i)*y2;                             % points in y
-        pointTraj(3,i) = (1-s(i))*z1 + s(i)*z2;                             % points in z
-    end    
-    
-    T = transl(pointTraj(1,1), pointTraj(2,1), pointTraj(3,1)) * trotx(pi); % transformation matrix with z pointing down
-    qMatrix(1,:) = robot.ikcon(T,q0);                                       % solve joint angles for 1st waypoint
-    robot.plot(qMatrix(1,:));                                               % plot initial joints
+        pointTraj(1,i) = (1-s(i))*x0 + s(i)*x1;                             % points in x
+        pointTraj(2,i) = (1-s(i))*y0 + s(i)*y1;                             % points in y
+        pointTraj(3,i) = (1-s(i))*z0 + s(i)*z1;                             % points in z
+    end        
+    qMatrix(1,:) = q0;                                                       % solve joint angles for 1st waypoint
 
     %% track trajectory with RMRC
     for i=1:steps-1
         T = robot.fkine(qMatrix(i,:));                                      % fkine transform at current joints
         deltaPos = pointTraj(:,i+1) - T(1:3,4);                             % position error from next waypoint
-        rotDesired = rotx(pi);                                              % desired rotation matrix (z pointing down)
+        rotDesired = roty(pi);                                              % desired rotation matrix (z pointing down)
         rotActual = T(1:3,1:3);                                             % actual rotation matrix (current end-effector rotation matrix)
         rotError = (1/deltaT)*(rotDesired - rotActual);                     % rotation matrix error
         S = rotError * rotActual';                                          % skew symmetric matrix
-        linearVelocity = deltaPos / deltaT;                                 % linear velocity
-        angularVelocity = [S(3,2); S(1,3); S(2,1)];                         % angular velocity
+        linear_velocity = (1/deltaT) * deltaPos;                            % linear velocity
+        angular_velocity = [S(3,2); S(1,3); S(2,1)];                         % angular velocity
         rollPitchYaw = tr2rpy(rotDesired*rotActual);                        % roll-pitch-yaw of rotation matrix
-        endEffectorVel = W*[linear_velocity;angular_velocity];              % end-effector velocity to reach next waypoint
+        endEffectorVel = W * [linear_velocity;angular_velocity];              % end-effector velocity to reach next waypoint
         J = robot.jacob0(qMatrix(i,:));                                     % jacobian at current joint state
         m(i) = sqrt(det(J*J'));                                             % calculate manipulability
 
@@ -52,7 +48,7 @@ function [qMatrix, posError, angleError] = solveRMRC(robot, point1, point2, q0, 
         else
             lambda = 0;                                                     % sufficient manipulability, no damping needed
         end
-        
+
         invJ = inv(J'*J + lambda * eye(numJoints)) * J';                    % damped least squares inverse
         qdot(i,:) = (invJ*endEffectorVel)';                                 % solve RMRC equation
         
