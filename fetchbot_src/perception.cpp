0927192e34628150c7c64b7569486d5f46943f58
brenmun00@gmail.com <brenmun00@gmail.com>
2022-05-14 01:23:08 +1000

    name of node is perception

--- fetchbot_src/perception.cpp
+++ fetchbot_src/perception.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char **argv)
 {
   // Look at table
-  ros::init(argc, argv, "robot_driver");
+  ros::init(argc, argv, "perception");
   RobotHead head;
   head.lookAt("base_link", 1, 0, 0.4); //frame and (x,y,z)
   ros::Duration(1);
